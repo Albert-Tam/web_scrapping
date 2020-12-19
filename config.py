2e8dfacf371f53109cfcd6a224b43d6e5f04c0b6
Albert_Tamman <tammanalbert@me.com>
2020-12-19 16:40:55 +0200

    config file changes 3

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class conf:
     host = 'ip-172-31-17-9'
-    user = 'root'
+    user = 'root@localhost'
     password = '12345678'
     api_key = '660c1e96d8msh1a93f181e274974p125202jsn75c41d0d3902'
